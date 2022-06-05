ea587287ea940cbf8544f65b1f678436961d8399
gssabu <87628438+gssabu@users.noreply.github.com>
2022-06-05 03:01:32 -0500

    Update firmware.ino

--- firmware/src/firmware.ino
+++ firmware/src/firmware.ino
@@ -94,7 +94,7 @@ Kinematics kinematics(
 Odometry odometry;
 IMU imu;
 
-void battstate_msg_init(sensor_msgs::msg::BatteryState &battstate_msg, char *frame_id_name)
+/*void battstate_msg_init(sensor_msgs::msg::BatteryState &battstate_msg, char *frame_id_name)
 {   
     // Populate battery parameters.
     battstate_msg.header.frame_id          = frame_id_name;
@@ -114,7 +114,7 @@ void range_msg_init(sensor_msgs::msg::Range &range_msg, char *frame_id_name)
     range_msg.field_of_view = 0.25;
     range_msg.min_range = 0.02;
     range_msg.max_range = 0.02;
-}
+}*/
 
 void setup() 
 {   
@@ -131,6 +131,22 @@ void setup()
     }
     micro_ros_init_successful = false;
     set_microros_transports();
+    
+    battstate_msg.header.frame_id          = "";
+    battstate_msg.design_capacity          = 2500;  // mAh
+    battstate_msg.power_supply_status      = 2;     // discharging
+    battstate_msg.power_supply_health      = 0;     // unknown
+    battstate_msg.power_supply_technology  = 2;     // Lion
+    battstate_msg.present                  = 1;     // battery present
+    battstate_msg.location.data            = "Linorobot2";        // unit location
+    
+    range_msg.radiation_type = sensor_msgs::msg::Range::INFRARED;
+    range_msg.header.frame_id = "/base_link";
+    range_msg.field_of_view = 0.25;
+    range_msg.min_range = 0.02;
+    range_msg.max_range = 0.02;
+    
+    
 }
 
 void loop() 
@@ -148,6 +164,8 @@ void loop()
             if (!micro_ros_init_successful) 
             {
                 createEntities();
+                ReadBatt();
+                ReadIr();
             } 
         } 
         else if(micro_ros_init_successful)
@@ -278,7 +296,7 @@ void createEntities()
     // create Batterystate publisher
     RCCHECK(rclc_publisher_init_default( 
         &battstate_publisher, 
-        &battstate_msg_init,
+        &node,
         ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, BatteryState),
         "BatteryState"
     ));
@@ -292,7 +310,7 @@ void createEntities()
     // create Range publisher
     RCCHECK(rclc_publisher_init_default( 
         &range_publisher, 
-        &range_msg_init,
+        &node,
         ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, Range),
         "Range"
     ));
